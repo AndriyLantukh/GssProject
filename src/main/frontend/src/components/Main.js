ae9fc8520f370c54750627020a29a155169cd6e0
lantukh <lantukh@is-mc-Lanukh-7.local>
2016-12-26 11:35:37 +0200

    switched  to ajax requests

--- src/main/frontend/src/components/Main.js
+++ src/main/frontend/src/components/Main.js
@@ -25,10 +25,10 @@ class Main extends React.Component {
 
     }
 
-    // componentDidMount() {
-    //     this.updateUsers();
-    //     this.updateCustomers();
-    // }
+    componentDidMount() {
+        this.updateUsers();
+        this.updateCustomers();
+    }
 
     updateUsers() {
         $.get("/users", (data) => {
@@ -46,75 +46,75 @@ class Main extends React.Component {
 
     onAddUser(login, password, role) {
         //   STUB
-        console.log("Add user");
-        let newUsers = this.state.users.slice();
-        newUsers.push({
-            id: newUsers.length,
-            login: login,
-            password: password,
-            role: role
-        });
-        this.setState({users: newUsers});
-
-        // $.ajax({
-        //     headers: {
-        //         "Accept": "application/json",
-        //         "Content-Type": "application/json"
-        //     },
-        //     type: "POST",
-        //     url: "/users/add",
-        //     data: JSON.stringify({login: login, password: password, role: role}),
-        //     success: () => this.updateUsers(),
-        //     error: () => this.updateUsers()
+        // console.log("Add user");
+        // let newUsers = this.state.users.slice();
+        // newUsers.push({
+        //     id: newUsers.length,
+        //     login: login,
+        //     password: password,
+        //     role: role
         // });
+        // this.setState({users: newUsers});
+
+        $.ajax({
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "/users/add",
+            data: JSON.stringify({login: login, password: password, role: role}),
+            success: () => this.updateUsers(),
+            error: () => this.updateUsers()
+        });
     }
 
     onDeleteUser(id) {
         //   STUB
-        console.log("Delete confirm" + id);
-        let newUsers = this.state.users.slice();
-        let index = newUsers.indexOf(newUsers.find(user => user.id === id));
-        newUsers.splice(index, 1);
-        this.setState({users: newUsers});
+        // console.log("Delete confirm" + id);
+        // let newUsers = this.state.users.slice();
+        // let index = newUsers.indexOf(newUsers.find(user => user.id === id));
+        // newUsers.splice(index, 1);
+        // this.setState({users: newUsers});
 
-       //      $.post("/users/delete/" + id, null, () => this.updateUsers);
+             $.post("/users/delete/" + id, null, () => this.updateUsers);
     }
 
     onAddCustomer(customerName, cellNumber, city, newPostOfficeNumber, balance, additionalInfo) {
         //STUB
-        console.log("Add customer");
-
-        let newCustomers = this.state.customers.slice();
-        newCustomers.push({
-            id: newCustomers.length,
-            customerName: customerName,
-            cellNumber: cellNumber,
-            city: city,
-            newPostOfficeNumber: newPostOfficeNumber,
-            balance: balance,
-            additionalInfo: additionalInfo,
-            orders: []
-        });
-        this.setState({customers: newCustomers});
-
-        // $.ajax({
-        //     headers: {
-        //         "Accept": "application/json",
-        //         "Content-Type": "application/json"
-        //     },
-        //     type: "POST",
-        //     url: "/customers/add",
-        //     data: JSON.stringify({
-        //         customerName: customerName,
-        //         cellNumber: cellNumber,
-        //         city: city,
-        //         newPostOfficeNumber: newPostOfficeNumber,
-        //         balance: balance,
-        //         additionalInfo: additionalInfo,
-        //     }),
-        //     success: () => this.updateCustomers(),
-        //     error: () => this.updateCustomers()
+        // console.log("Add customer");
+        //
+        // let newCustomers = this.state.customers.slice();
+        // newCustomers.push({
+        //     id: newCustomers.length,
+        //     customerName: customerName,
+        //     cellNumber: cellNumber,
+        //     city: city,
+        //     newPostOfficeNumber: newPostOfficeNumber,
+        //     balance: balance,
+        //     additionalInfo: additionalInfo,
+        //     orders: []
         // });
+        // this.setState({customers: newCustomers});
+
+        $.ajax({
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            type: "POST",
+            url: "/customers/add",
+            data: JSON.stringify({
+                customerName: customerName,
+                cellNumber: cellNumber,
+                city: city,
+                newPostOfficeNumber: newPostOfficeNumber,
+                balance: balance,
+                additionalInfo: additionalInfo,
+            }),
+            success: () => this.updateCustomers(),
+            error: () => this.updateCustomers()
+        });
     }
 
     render() {
